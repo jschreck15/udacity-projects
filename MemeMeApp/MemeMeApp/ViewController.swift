b6537b1104a91cf74f41eac4cd2f23870e7d9af0
Jorden Schreck <jorden@Jordens-MacBook-Air.local>
2015-06-26 14:03:31 -0600

    Add MemeME App to git repo

---
+++ MemeMeApp/MemeMeApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MemeMeApp
+//
+//  Created by Jorden Schreck on 6/26/15.
+//  Copyright (c) 2015 Jorden Schreck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func pickAnImage(sender: AnyObject) {
+    }
+
+}
+
